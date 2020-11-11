966f5fde5d651f739b444a1ac1e3354e5de3661a
JUNGYUN-Daegu/JUNGYUN-Daegu/jungyun <tgms021@gmail.com>
2020-11-11 17:03:05 +0900

    Exercise4: TypingPracticeApp - solved

--- 201111_week2_Practice/TypingPracticeApp/TypingPracticeApp/ViewController.swift
+++ 201111_week2_Practice/TypingPracticeApp/TypingPracticeApp/ViewController.swift
@@ -8,12 +8,43 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var textArray = ["Apple", "Grape", "Orange", "Banana", "Kiwi", "Peach", "Durian", "Out", "Of", "Idea"]
+    
+    @IBOutlet weak var currentTextField: UILabel!
+    @IBOutlet weak var nextTextField: UILabel!
+    @IBOutlet weak var inputField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        currentTextField.text = textArray.randomElement()
+        nextTextField.text = textArray.randomElement()
+        
+        // handle duplicated element at each of the textFields
+        while (currentTextField.text == nextTextField.text) {
+            nextTextField.text = textArray.randomElement()
+        }
+        displayedTextDelete()
     }
 
-
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        if inputField.text == currentTextField.text {
+            currentTextField.text = nextTextField.text
+            nextTextField.text = textArray.randomElement()
+            inputField.text = ""
+            displayedTextDelete()
+        } else {
+            inputField.text = ""
+        }
+    }
+    
+    // remove currently being displayed Strings from textArray
+    func displayedTextDelete() {
+        if let index = textArray.firstIndex(of: currentTextField.text ?? "") {
+            textArray.remove(at: index)
+        }
+        if let index = textArray.firstIndex(of: nextTextField.text ?? "") {
+            textArray.remove(at: index)
+        }
+    }
 }
 
