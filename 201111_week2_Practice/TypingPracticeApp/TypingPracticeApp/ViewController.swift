f6703fc100f6e19e54354f6e28e4248459a5ea8a
JUNGYUN-Daegu/JUNGYUN-Daegu/jungyun <tgms021@gmail.com>
2020-11-11 16:03:06 +0900

    Exercise4: TypingPracticeApp - Initial Commit

---
+++ 201111_week2_Practice/TypingPracticeApp/TypingPracticeApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TypingPracticeApp
+//
+//  Created by 조중윤 on 2020/11/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
