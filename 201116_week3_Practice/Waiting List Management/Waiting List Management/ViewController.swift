dfb6a5f097732c5a2d5f1ec2397214c61591a974
JUNGYUN-Daegu/JUNGYUN-Daegu/jungyun <tgms021@gmail.com>
2020-11-16 16:32:11 +0900

    Exercise4: Waiting List Management - Initial Commit

---
+++ 201116_week3_Practice/Waiting List Management/Waiting List Management/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Waiting List Management
+//
+//  Created by 조중윤 on 2020/11/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
