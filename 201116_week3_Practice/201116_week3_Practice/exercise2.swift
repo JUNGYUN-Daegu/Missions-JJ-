aaa4bcc284a6e515af985276f4a0c66c7c0afb53
JUNGYUN-Daegu/JUNGYUN-Daegu/jungyun <tgms021@gmail.com>
2020-11-16 14:37:21 +0900

    Exercise2: Binary Number Structure - solved with possibility to improve

---
+++ 201116_week3_Practice/201116_week3_Practice/exercise2.swift
@@ -0,0 +1,45 @@
+//
+//  exercise2.swift
+//  201116_week3_Practice
+//
+//  Created by 조중윤 on 2020/11/16.
+//
+
+import Foundation
+
+//FIXME:- how to make all the binary numbers have 5 digits?
+//Binary Number Structure
+struct SpecialBinaryNumber {
+    private var valueArray: Array<String> = []
+    // count 프로퍼티 private 접근자 사용시 main 파일에서 해당 값 열람 불가능
+    var count: Int {
+        return valueArray.count
+    }
+    
+    init(with total: Int) {
+        for _ in 1...50 {
+            // if binary code is 5 digits, the max value of it in decimal is 2^5 - 1.
+            let maxValue = pow(2, total) - 1
+            // putting maxValue(decimal) into NSDecimalNumber so that later it can be type casted into Int?
+            let valueDecimal = NSDecimalNumber(decimal: maxValue)
+            let maxInt = Int(truncating: valueDecimal)
+            let randomNumber: Int = Int.random(in: 0...maxInt)
+            // radix?
+            let binaryString = String(randomNumber, radix: 2)
+            valueArray.append(binaryString)
+        }
+    }
+    
+    func find(by bitCount: Int) -> Array<String> {
+        let result = valueArray.filter { (string) -> Bool in
+            var numberOfOne = 0
+            for letter in string {
+                if letter == "1" {
+                    numberOfOne += 1
+                }
+            }
+           return numberOfOne == bitCount
+        }
+        return result
+    }
+}
--- 201116_week3_Practice/201116_week3_Practice/main.swift
+++ 201116_week3_Practice/201116_week3_Practice/main.swift
@@ -8,11 +8,16 @@
 import Foundation
 
 //MARK:- EXERCISE 1
-let myMatrix1 = Matrix(a: 7, b: 6, c: 5, d: 4)
-let myMatrix2 = Matrix(a: 4, b: 5, c: 6, d: 7)
+//let myMatrix1 = Matrix(a: 7, b: 6, c: 5, d: 4)
+//let myMatrix2 = Matrix(a: 4, b: 5, c: 6, d: 7)
+//
+//let summedMatrix: Matrix = myMatrix1.sum(with: myMatrix2)
+//let multipliedMatrix: Matrix = myMatrix1.product(with: myMatrix2)
+//
+//print("\(summedMatrix.a) | \(summedMatrix.b)\n\(summedMatrix.c) | \(summedMatrix.d)")
+//print("\(multipliedMatrix.a) | \(multipliedMatrix.b)\n\(multipliedMatrix.c) | \(multipliedMatrix.d)")
 
-let summedMatrix: Matrix = myMatrix1.sum(with: myMatrix2)
-let multipliedMatrix: Matrix = myMatrix1.product(with: myMatrix2)
-
-print("\(summedMatrix.a) | \(summedMatrix.b)\n\(summedMatrix.c) | \(summedMatrix.d)")
-print("\(multipliedMatrix.a) | \(multipliedMatrix.b)\n\(multipliedMatrix.c) | \(multipliedMatrix.d)")
+//MARK:- EXERCISE 2
+var mySpecialBinaryNumber = SpecialBinaryNumber(with: 5)
+print(mySpecialBinaryNumber.count)
+print(mySpecialBinaryNumber.find(by: 3))
