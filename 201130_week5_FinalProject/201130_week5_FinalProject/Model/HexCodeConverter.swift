7bbe3735a3105c9a5b9a640efcb47c14321300f2
JUNGYUN-Daegu/JUNGYUN-Daegu/jungyun <tgms021@gmail.com>
2020-11-30 14:12:12 +0900

    UIColor extension: hexString2UIColor

--- 201130_week5_FinalProject/201130_week5_FinalProject/Model/HexCodeConverter.swift
+++ 201130_week5_FinalProject/201130_week5_FinalProject/Model/HexCodeConverter.swift
@@ -6,3 +6,43 @@
 //
 
 import UIKit
+
+extension UIColor {
+    class func splitHexColorCode(hexColorString: String) -> [String]? {
+        // 문자열을 배열화 하면 문자 배열([Character])이 된다.
+        let array = Array<Character>(hexColorString)
+        var result = [String]()
+
+        if array.count == 7 {
+            result.append(String(array[0]))
+            result.append("\(array[1])\(array[2])")
+            result.append("\(array[3])\(array[4])")
+            result.append("\(array[5])\(array[6])")
+            return result
+           
+        } else if array.count == 4 {
+            result.append(String(array[0]))
+            result.append("\(array[1])\(array[1])")
+            result.append("\(array[2])\(array[2])")
+            result.append("\(array[3])\(array[3])")
+            return result
+        } else {
+            return nil
+        }
+    }
+    class func hexString2CGFloat(hexString: String) -> CGFloat {
+           let scanner = Scanner(string: hexString)
+           var intValue: UInt64 = 0
+           scanner.scanHexInt64(&intValue)
+           return CGFloat(CGFloat(intValue) / 255.0)
+    }
+    class func hexString2UIColor(hexString: String) -> UIColor? {
+        if let hexStrings = splitHexColorCode(hexColorString: hexString) {
+            let r = hexString2CGFloat(hexString: hexStrings[1])
+            let g = hexString2CGFloat(hexString: hexStrings[2])
+            let b = hexString2CGFloat(hexString: hexStrings[3])
+            return UIColor(red: r, green: g, blue: b, alpha: 1.0)
+        }
+        return nil
+    }
+}
